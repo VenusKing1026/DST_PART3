11600b388e423b6a67af497bce26fc34c2eda750
XiaoyuYE123 <xiaoyu.24@intl.zju.edu.cn>
2026-04-02 14:37:09 +0800

    加入Annovar

--- src/main/java/cn/edu/zju/dao/AnnovarDao.java
+++ src/main/java/cn/edu/zju/dao/AnnovarDao.java
@@ -20,7 +20,19 @@ public class AnnovarDao extends BaseDao {
         }
 
         DBUtils.execSQL(connection -> {
-            String sql = "INSERT INTO annovar (sample_id, Chr, Start, End, Ref, Alt, `Func.refGene`, `Gene.refGene`, `GeneDetail.refGene`, `ExonicFunc.refGene`, `AAChange.refGene`, cytoBand, `1000g2015aug_all`, `1000g2015aug_afr`, `1000g2015aug_amr`, `1000g2015aug_eas`, `1000g2015aug_eur`, `1000g2015aug_sas`, ExAC_ALL, ExAC_AFR, ExAC_AMR, ExAC_EAS, ExAC_FIN, ExAC_NFE, ExAC_OTH, ExAC_SAS, avsnp150, esp6500siv2_all, esp6500siv2_ea, esp6500siv2_aa, gnomAD_exome_ALL, gnomAD_exome_AFR, gnomAD_exome_AMR, gnomAD_exome_ASJ, gnomAD_exome_EAS, gnomAD_exome_FIN, gnomAD_exome_NFE, gnomAD_exome_OTH, gnomAD_exome_SAS, SIFT_score, SIFT_converted_rankscore, SIFT_pred, Polyphen2_HDIV_score, Polyphen2_HDIV_rankscore, Polyphen2_HDIV_pred, Polyphen2_HVAR_score, Polyphen2_HVAR_rankscore, Polyphen2_HVAR_pred, LRT_score, LRT_converted_rankscore, LRT_pred, MutationTaster_score, MutationTaster_converted_rankscore, MutationTaster_pred, MutationAssessor_score, MutationAssessor_score_rankscore, MutationAssessor_pred, FATHMM_score, FATHMM_converted_rankscore, FATHMM_pred, PROVEAN_score, PROVEAN_converted_rankscore, PROVEAN_pred, VEST3_score, VEST3_rankscore, MetaSVM_score, MetaSVM_rankscore, MetaSVM_pred, MetaLR_score, MetaLR_rankscore, MetaLR_pred, `M-CAP_score`, `M-CAP_rankscore`, `M-CAP_pred`, REVEL_score, REVEL_rankscore, MutPred_score, MutPred_rankscore, CADD_raw, CADD_raw_rankscore, CADD_phred, DANN_score, DANN_rankscore, `fathmm-MKL_coding_score`, `fathmm-MKL_coding_rankscore`, `fathmm-MKL_coding_pred`, Eigen_coding_or_noncoding, `Eigen-raw`, `Eigen-PC-raw`, GenoCanyon_score, GenoCanyon_score_rankscore, integrated_fitCons_score, integrated_fitCons_score_rankscore, integrated_confidence_value, `GERP++_RS`, `GERP++_RS_rankscore`, phyloP100way_vertebrate, phyloP100way_vertebrate_rankscore, phyloP20way_mammalian, phyloP20way_mammalian_rankscore, phastCons100way_vertebrate, phastCons100way_vertebrate_rankscore, phastCons20way_mammalian, phastCons20way_mammalian_rankscore, SiPhy_29way_logOdds, SiPhy_29way_logOdds_rankscore, Interpro_domain, GTEx_V6p_gene, GTEx_V6p_tissue, gnomAD_genome_ALL, gnomAD_genome_AFR, gnomAD_genome_AMR, gnomAD_genome_ASJ, gnomAD_genome_EAS, gnomAD_genome_FIN, gnomAD_genome_NFE, gnomAD_genome_OTH, CLNALLELEID, CLNDN, CLNDISDB, CLNREVSTAT, CLNSIG, cosmic70, ICGC_Id, ICGC_Occurrence, InterVar_automated, PVS1, PS1, PS2, PS3, PS4, PM1, PM2, PM3, PM4, PM5, PM6, PP1, PP2, PP3, PP4, PP5, BA1, BS1, BS2, BS3, BS4, BP1, BP2, BP3, BP4, BP5, BP6, BP7, Otherinfo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+            // 只插入当前 AnnovarService 实际会产生的字段
+            String sql = "INSERT INTO annovar (" +
+                    "sample_id, " +
+                    "Chr, Start, End, Ref, Alt, " +
+                    "`Func.refGene`, `Gene.refGene`, `GeneDetail.refGene`, `ExonicFunc.refGene`, `AAChange.refGene`, " +
+                    "cytoBand, " +
+                    "`1000g2015aug_all`, `1000g2015aug_afr`, `1000g2015aug_amr`, `1000g2015aug_eas`, `1000g2015aug_eur`, `1000g2015aug_sas`, " +
+                    "CLNALLELEID, CLNDN, CLNDISDB, CLNREVSTAT, CLNSIG, " +
+                    "Otherinfo" +
+                    ") VALUES (" +
+                    "?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?" +
+                    ")";
 
             try {
                 connection.setAutoCommit(false);
@@ -33,53 +45,13 @@ public class AnnovarDao extends BaseDao {
                     headerIndex.put(headers[i], i);
                 }
 
-                // 数据库字段顺序（不含 sample_id，最后含 Otherinfo）
+                // 当前版本 runAnnovar() 实际会返回的固定字段
                 List<String> dbFields = Arrays.asList(
                         "Chr", "Start", "End", "Ref", "Alt",
                         "Func.refGene", "Gene.refGene", "GeneDetail.refGene", "ExonicFunc.refGene", "AAChange.refGene",
                         "cytoBand",
                         "1000g2015aug_all", "1000g2015aug_afr", "1000g2015aug_amr", "1000g2015aug_eas", "1000g2015aug_eur", "1000g2015aug_sas",
-                        "ExAC_ALL", "ExAC_AFR", "ExAC_AMR", "ExAC_EAS", "ExAC_FIN", "ExAC_NFE", "ExAC_OTH", "ExAC_SAS",
-                        "avsnp150", "esp6500siv2_all", "esp6500siv2_ea", "esp6500siv2_aa",
-                        "gnomAD_exome_ALL", "gnomAD_exome_AFR", "gnomAD_exome_AMR", "gnomAD_exome_ASJ", "gnomAD_exome_EAS",
-                        "gnomAD_exome_FIN", "gnomAD_exome_NFE", "gnomAD_exome_OTH", "gnomAD_exome_SAS",
-                        "SIFT_score", "SIFT_converted_rankscore", "SIFT_pred",
-                        "Polyphen2_HDIV_score", "Polyphen2_HDIV_rankscore", "Polyphen2_HDIV_pred",
-                        "Polyphen2_HVAR_score", "Polyphen2_HVAR_rankscore", "Polyphen2_HVAR_pred",
-                        "LRT_score", "LRT_converted_rankscore", "LRT_pred",
-                        "MutationTaster_score", "MutationTaster_converted_rankscore", "MutationTaster_pred",
-                        "MutationAssessor_score", "MutationAssessor_score_rankscore", "MutationAssessor_pred",
-                        "FATHMM_score", "FATHMM_converted_rankscore", "FATHMM_pred",
-                        "PROVEAN_score", "PROVEAN_converted_rankscore", "PROVEAN_pred",
-                        "VEST3_score", "VEST3_rankscore",
-                        "MetaSVM_score", "MetaSVM_rankscore", "MetaSVM_pred",
-                        "MetaLR_score", "MetaLR_rankscore", "MetaLR_pred",
-                        "M-CAP_score", "M-CAP_rankscore", "M-CAP_pred",
-                        "REVEL_score", "REVEL_rankscore", "MutPred_score", "MutPred_rankscore",
-                        "CADD_raw", "CADD_raw_rankscore", "CADD_phred",
-                        "DANN_score", "DANN_rankscore",
-                        "fathmm-MKL_coding_score", "fathmm-MKL_coding_rankscore", "fathmm-MKL_coding_pred",
-                        "Eigen_coding_or_noncoding", "Eigen-raw", "Eigen-PC-raw",
-                        "GenoCanyon_score", "GenoCanyon_score_rankscore",
-                        "integrated_fitCons_score", "integrated_fitCons_score_rankscore", "integrated_confidence_value",
-                        "GERP++_RS", "GERP++_RS_rankscore",
-                        "phyloP100way_vertebrate", "phyloP100way_vertebrate_rankscore",
-                        "phyloP20way_mammalian", "phyloP20way_mammalian_rankscore",
-                        "phastCons100way_vertebrate", "phastCons100way_vertebrate_rankscore",
-                        "phastCons20way_mammalian", "phastCons20way_mammalian_rankscore",
-                        "SiPhy_29way_logOdds", "SiPhy_29way_logOdds_rankscore",
-                        "Interpro_domain", "GTEx_V6p_gene", "GTEx_V6p_tissue",
-                        "gnomAD_genome_ALL", "gnomAD_genome_AFR", "gnomAD_genome_AMR", "gnomAD_genome_ASJ",
-                        "gnomAD_genome_EAS", "gnomAD_genome_FIN", "gnomAD_genome_NFE", "gnomAD_genome_OTH",
-                        "CLNALLELEID", "CLNDN", "CLNDISDB", "CLNREVSTAT", "CLNSIG",
-                        "cosmic70", "ICGC_Id", "ICGC_Occurrence",
-                        "InterVar_automated",
-                        "PVS1", "PS1", "PS2", "PS3", "PS4",
-                        "PM1", "PM2", "PM3", "PM4", "PM5", "PM6",
-                        "PP1", "PP2", "PP3", "PP4", "PP5",
-                        "BA1",
-                        "BS1", "BS2", "BS3", "BS4",
-                        "BP1", "BP2", "BP3", "BP4", "BP5", "BP6", "BP7"
+                        "CLNALLELEID", "CLNDN", "CLNDISDB", "CLNREVSTAT", "CLNSIG"
                 );
 
                 Set<String> knownFields = new HashSet<>(dbFields);
@@ -89,30 +61,43 @@ public class AnnovarDao extends BaseDao {
                         continue;
                     }
 
-                    String[] split = lines[i].split("\\t", -1);
-
-                    preparedStatement.setInt(1, sampleId);
-
-                    // 按列名填充 2~154
-                    for (int j = 0; j < dbFields.size(); j++) {
-                        String fieldName = dbFields.get(j);
-                        preparedStatement.setString(j + 2, getValue(split, headerIndex, fieldName));
+                    // 跳过 VCF 头信息混入的行
+                    if (lines[i].startsWith("##") || lines[i].startsWith("#CHROM")) {
+                        continue;
                     }
 
-                    // 收集不在数据库固定字段中的列，拼到 Otherinfo
+                    String[] split = lines[i].split("\\t", -1);
+
+                    // 如果连最基本的变异列都没有，跳过
+                    if (split.length < 5) {
+                        continue;
+                    }
+
+                    // 第1列：sample_id
+                    preparedStatement.setInt(1, sampleId);
+
+                    // 第2列开始：固定字段
+                    for (int j = 0; j < dbFields.size(); j++) {
+                        String fieldName = dbFields.get(j);
+                        preparedStatement.setString(j + 2, normalizeValue(getValue(split, headerIndex, fieldName)));
+                    }
+
+                    // 其余所有非固定字段，统一拼到 Otherinfo
                     StringJoiner otherInfo = new StringJoiner("\t");
                     for (int h = 0; h < headers.length; h++) {
                         String header = headers[h];
                         if (!knownFields.contains(header)) {
                             String value = h < split.length ? split[h] : "";
-                            otherInfo.add(header + "=" + value);
+                            otherInfo.add(header + "=" + normalizeValue(value));
                         }
                     }
-                    preparedStatement.setString(155, otherInfo.toString());
+
+                    // 最后一列：Otherinfo
+                    preparedStatement.setString(dbFields.size() + 2, otherInfo.toString());
 
                     preparedStatement.addBatch();
 
-                    if (i % 1000 == 0) {
+                    if (i % 500 == 0) {
                         preparedStatement.executeBatch();
                         connection.commit();
                     }
@@ -135,8 +120,21 @@ public class AnnovarDao extends BaseDao {
         return split[idx];
     }
 
+    private String normalizeValue(String value) {
+        if (value == null) {
+            return "";
+        }
+        value = value.trim();
+        return ".".equals(value) ? "" : value;
+    }
+
     public List<String> getRefGenes(int sampleId) {
-        String sql = "select distinct `Gene.refGene` from annovar where `ExonicFunc.refGene` != 'synonymous SNV' and sample_id = ?";
+        String sql = "select distinct `Gene.refGene` from annovar " +
+                "where sample_id = ? " +
+                "and `Gene.refGene` is not null " +
+                "and `Gene.refGene` <> '' " +
+                "and (`ExonicFunc.refGene` is null or `ExonicFunc.refGene` <> 'synonymous SNV')";
+
         List<String> genes = new ArrayList<>();
         DBUtils.execSQL(connection -> {
             try {
